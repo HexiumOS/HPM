1a41f3e0a3eaebdfbf2ad61c7a0fb40dd1695aad
Viktor Popp <viktorpopp.dev@gmail.com>
2025-08-27 20:37:15 +0200

    Back to the beginning

--- src/commands/install.rs
+++
@@ -1,5 +0,0 @@
-const SYMLINK_DIR: &str = "~/.local/bin/";
-const BINRARY_DIR: &str = "~/.local/lib/hpm/packages/";
-const TEMPORARY_DIR: &str = "~/.local/lib/hpm/temporary/";
-
-pub fn install(package: &str) {}
